ab89b97eca1fbe79f8aaa1668db11e8ab685fcbd
dnlcesilva <dnlcesilva@gmail.com>
2021-01-20 13:40:05 -0300

    modificado

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 
-'''
 nome = input("Digite seu nome: \n")
 idade = int(input("Digite sua idade:\n"))
 sobrenome = input("Digite seu sobrenome:\n")
 
 print(f"Olá, {nome} {sobrenome}, sua idade é {idade} anos!!")
 print(type(idade))
-
+'''
 linguagem, nivel, companhia = input().split()
 
 print(linguagem)
 print(nivel)
 print(companhia)
 '''
+if idade >= 18:
+    print(f"{nome} é maior de idade")
+else:
+    print(f"{nome} é menor de idade")
+
 

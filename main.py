f9f216bf80e553c8aac416b906c52e515ce0fcda
dnlcesilva <dnlcesilva@gmail.com>
2021-01-20 13:16:34 -0300

    Add novos arquivos

---
+++ main.py
@@ -0,0 +1,16 @@
+
+'''
+nome = input("Digite seu nome: \n")
+idade = int(input("Digite sua idade:\n"))
+sobrenome = input("Digite seu sobrenome:\n")
+
+print(f"Olá, {nome} {sobrenome}, sua idade é {idade} anos!!")
+print(type(idade))
+
+linguagem, nivel, companhia = input().split()
+
+print(linguagem)
+print(nivel)
+print(companhia)
+'''
+
